1104c9fa1290d46c5b23643bfa074554f7b71079
VENKATESH SUBRAMANIAN <venh.123@gmail.com>
2023-05-22 11:09:59 +0530

    Update printMsg.groovy

--- vars/printMsg.groovy
+++ vars/printMsg.groovy
@@ -1,7 +1,7 @@
-def printMsg(String jobName) {
+def printMessage(String jobName) {
   message = "Hi " + jobName + ". This is from Shared Library."
 }
 
 def call (String jobName) {
-   printMsg(to, from, subject, body)
+   printMessage(jobName)
 }
